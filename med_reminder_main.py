818481e0669c18dfff9127df9451e7a74eb58847
Chris Hodapp <hodapp87@gmail.com>
2020-10-31 11:50:37 -0400

    Quick asyncio example

--- med_reminder_main.py
+++ med_reminder_main.py
@@ -1,7 +1,9 @@
+import asyncio
+
 import lcddriver
 import time
 import RPi.GPIO as GPIO
-import schedule
+#import schedule
 
 display = lcddriver.lcd()
 redLed = 4
@@ -16,31 +18,33 @@ GPIO.setup(yellowLed, GPIO.OUT)
 med1_line1 = "Ibuprofen 200mg"
 med1_line2 = "2 tablet  Bin A"
 
-def lightFlash():
+async def lightFlash():
     while True:
         GPIO.output(redLed, True)
-        time.sleep(1)
+        await asyncio.sleep(1)
         GPIO.output(redLed, False)
+        await asyncio.sleep(1)
 
+async def start():
+    try:
+        asyncio.create_task(lightFlash())
+        display.lcd_backlight(0)
+        # Reminder 16 character long sentences!
+        GPIO.output(redLed, True)
+        display.lcd_backlight(0)
+        print("Writing to display")
+        display.lcd_display_string(med1_line1, 1)
+        display.lcd_display_string(med1_line2, 2)
+        await asyncio.sleep(lcd_display_time)
 
-try:
-    lightFlash()
-    display.lcd_backlight(0)
-    # Reminder 16 character long sentences!
-    GPIO.output(redLed, True)
-    display.lcd_backlight(0)
-    print("Writing to display")
-    display.lcd_display_string(med1_line1, 1)
-    display.lcd_display_string(med1_line2, 2)
-    time.sleep(lcd_display_time)
-        
-    GPIO.output(redLed, False)
-    display.lcd_clear()
-    display.lcd_backlight(0)
-                
-except KeyboardInterrupt:
-    print("Cleaning up!")
-    display.lcd_clear()
-    display.lcd_backlight(0)
-    GPIO.cleanup() 
+        GPIO.output(redLed, False)
+        display.lcd_clear()
+        display.lcd_backlight(0)
+
+    except KeyboardInterrupt:
+        print("Cleaning up!")
+        display.lcd_clear()
+        display.lcd_backlight(0)
+        GPIO.cleanup() 
     
+asyncio.run(start())
